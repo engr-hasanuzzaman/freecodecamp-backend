fd1d27a82861d4e6584b1bb6a510a682bc204c82
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-13 19:58:27 +0600

    complete basic http get challenge

---
+++ basic-http-get.js
@@ -0,0 +1,13 @@
+var http = require('http')
+
+let url = process.argv[2]
+
+http.get(url, (res) => {
+  res.setEncoding('utf8')
+  res.on("data", (data) => {
+    console.log(data);
+  })
+  res.on("error", (error) => {
+    console.error(error);
+  })
+}).on('error', console.error)
