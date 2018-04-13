060c55e9c5e9be2686b32de45c71b5cdfe4e8a8a
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-13 23:06:52 +0600

    use concat-stream package

--- basic-http-get.js
+++ basic-http-get.js
@@ -1,13 +1,23 @@
 var http = require('http')
+const concat = require('concat-stream')
 
 let url = process.argv[2]
 
 http.get(url, (res) => {
   res.setEncoding('utf8')
+  contents = ''
   res.on("data", (data) => {
-    console.log(data);
+    contents += data
   })
+  res.pipe(concat((data) => {
+    console.log(data.length);
+    console.log(data);
+  }))
   res.on("error", (error) => {
     console.error(error);
+  })
+  res.on('end', (data) => {
+    // console.log(contents.length);
+    // console.log(contents);
   })
 }).on('error', console.error)
