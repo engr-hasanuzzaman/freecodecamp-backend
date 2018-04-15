129067a1ffaa50d769b411a81ef1d51bbc6966a6
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-15 13:56:11 +0600

    complete node-uppercase challenge

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -1,10 +1,37 @@
 const fs = require('fs')
 const http = require('http')
 const concat = require('concat-stream')
+var map = require('through2-map')  
 
+
+const PORT = process.argv[2]
 const server = http.createServer(reqHandler)
 
 function reqHandler(req, res) {
-  if(req.method === !'post')
-    return res.end('only post request is acceptable')
+  console.log("your request is ", req.method);
+  if(req.method !== 'POST'){
+    res.writeHead(500, { 'content-type': 'text/plain' });
+    return res.end('only post request is acceptable');
+  }
+
+  // solution using through2-map that run map on stream
+  // req.pipe(map(function (chunk) {  
+  //   return chunk.toString().toUpperCase(); 
+  // })).pipe(res);  
+  
+  let data = '';
+  // collect all data
+  req.on('data', (chunk) => {
+    data += chunk;
+  })
+
+  // send response
+  req.on('end', () => {
+    res.end(data.toUpperCase());
+  })
 }
+
+
+server.listen(PORT, () => {
+  console.log("node server has been started on port ", PORT);
+})
