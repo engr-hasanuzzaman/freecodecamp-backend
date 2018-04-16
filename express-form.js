e016228a4652a3ba81c7193e7552288a4ea486fd
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-16 17:30:57 +0600

    Complete use stylus challange
    install stylus npm

--- express-form.js
+++ express-form.js
@@ -4,17 +4,25 @@ var http = require('http');
 var path = require('path');
 var pug = require('pug');
 var morgan = require('morgan');
+var stylus = require('stylus');
 
 const app = express(); // response handler
 // view setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(stylus.middleware(path.join(__dirname, 'public/css')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/css')));
 
 app.use(morgan());
 app.use(bodyparser.urlencoded({extened: false}));
 app.get('/form', (req, res) =>{
     res.render('form', {});
 });
+
+app.get('/index.html', (req, res) => {
+    res.render('index', {});
+})
 
 // handle form submission
 app.post('*', (req, res) => {
