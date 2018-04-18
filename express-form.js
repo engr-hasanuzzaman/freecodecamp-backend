9186d0dcb8469f5f8f2c018a45309ea534c7e20d
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-18 21:35:44 +0600

    add missing files

--- express-form.js
+++ express-form.js
@@ -1,13 +1,34 @@
 var express = require('express');
+var bodyparser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var pug = require('pug');
 var morgan = require('morgan');
+var stylus = require('stylus');
 
 const app = express(); // response handler
 // view setup
-app.use(morgan());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(stylus.middleware(path.join(__dirname, 'public/css')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/css')));
 
-app.get('/search', (req, res) => {
-  res.end(req.query);
+app.use(morgan());
+app.use(bodyparser.urlencoded({extened: false}));
+app.get('/form', (req, res) =>{
+    res.render('form', {});
+});
+
+app.get('/index.html', (req, res) => {
+    res.render('index', {});
 })
+
+// handle form submission
+app.post('*', (req, res) => {
+  res.end(req.body.str.split('').reverse().join(''));
+});
+
 let port = (process.argv[2] || 3000);
 http.createServer(app).listen(port);
 console.log('server start on port: ', port);
