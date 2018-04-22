76c6beea0430131fe5d9c37d0a0a0e3a59bf24d9
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-22 13:51:22 +0600

    complete some practice and start writting todos app

--- koa1/rest.js
+++ koa1/rest.js
@@ -1,26 +1,29 @@
-const http = require('http');
-const  koa = require('koa');
+// const http = require('http');
+const koa = require('koa');
 const logger = require('koa-logger');
 const path = require('path');
 const serve = require('koa-static');
+const router = require('koa-router')();
 const app = new koa();
+const posts = [{ title: 'First post', content: 'This is default first post for testing purposes' }]; // database
 
 // setup middleware
 app.use(logger());
 app.use(serve(path.join(__dirname, 'public')));
-app.use((ctx) => {
-  switch (ctx.accepts('html', 'json')) {
-    case 'html':
-      ctx.type = 'html';
-      ctx.body = '<p> Wellcome to koa application</p>';
-      break;
-    case 'json':
-        ctx.type = 'json';
-        ctx.body = { msg: 'Wellcome to koa api' };
-        break;
-    default:
-      ctx.body = 'not found'
-  }
-})
+
+// foute defination
+router.get('/posts', showAllPosts);
+
+// apply routes to koa 
+app.use(router.routes());
+
+/**
+ * post listing 
+ */
+function showAllPosts(ctx){
+  ctx.type = 'json';
+  ctx.body = posts;
+}
+
 // request handle
 app.listen(3000);
