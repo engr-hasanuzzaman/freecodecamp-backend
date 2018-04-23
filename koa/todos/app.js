f5a4579e8a7ebbd1fb3c70637bb57641ec25c404
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-04-23 16:03:58 +0600

    add basic stylesheet

--- koa/todos/app.js
+++ koa/todos/app.js
@@ -2,6 +2,8 @@ const renderer = require('./lib/render');
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const koaBody = require('koa-body');
+const serve = require('koa-static');
+const path = require('path');
 
 const koa = require('koa');
 const app = module.exports = new koa();
@@ -13,13 +15,16 @@ const todos = [
 ];
 
 app.use(logger());
+app.use(serve(path.join(__dirname + '/public')));
 app.use(koaBody());
 app.use(renderer);
 
 // declare router
 router.get('/todos', todoIndex)
-  .get('/todos/:id', todoShow)
+  .get('/', todoIndex)
   .get('/todos/new', todoNew)
+  .get('/todos/:id', todoShow)
+  .post('/todos', todoCreate)
       
 app.use(router.routes());
 
@@ -27,19 +32,29 @@ app.use(router.routes());
  * 
  */
 async function todoIndex(ctx){
-  await ctx.render('todos/index', { todos: todos });
+  await ctx.render('todos/index', { todos: todos, title: 'index' });
 }
 
 async function todoShow(ctx){
   let id = ctx.params.id;
   let todo = todos[id];
   if(!todo) ctx.throw(404, 'content not found');
-  
-  await ctx.render('todos/show', { todo: todo });
+
+  await ctx.render('todos/show', { todo: todo, title: 'show todo' });
 }
 
 async function todoNew(ctx){
-  await ctx.render('todos/new');
+  await ctx.render('todos/new', { title: 'new todo' });
+}
+
+async function todoCreate(ctx){
+  console.log('---call create todo method--');
+  let todo = ctx.request.body;
+  todo.status = false;
+  todo.created_at = new Date();
+  todo.completed_at = null;
+  todos.push(todo);
+  ctx.redirect('/todos');
 }
 
 if(!module.parent){
